024986c6355b3ed2d353dfc59c9d78d3b29f2f01
Michael Bruins <Michael.S.Bruins@gmail.com>
2023-08-21 20:44:34 -0400

    added theme store

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Paper, ThemeProvider } from '@mui/material';
+import { theme } from './providers/theme';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Simple React Typescript Tailwind Sample
-      </h1>
-    </div>
+    <main>
+      <ThemeProvider theme={theme}>
+        <Paper>
+
+        </Paper>
+      </ThemeProvider>
+    </main>
   );
 }
 
