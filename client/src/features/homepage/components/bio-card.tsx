8b2d4fa724cbe2ec2ec56b246bf3d3324f24f849
Michael Bruins <Michael.S.Bruins@gmail.com>
2023-09-10 15:33:13 -0400

    added homepage layout and imports

---
+++ client/src/features/homepage/components/bio-card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card, CardContent, CardMedia } from '@mui/material';
+
+export const BioCard = () => {
+    return (
+        <Card>
+            <CardContent>
+                Hello!
+            </CardContent>
+        </Card>
+    )
+}
