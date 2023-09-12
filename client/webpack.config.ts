bcc08df3b1c88628d7b2aaac11fc4179ac255913
Michael Bruins <Michael.S.Bruins@gmail.com>
2023-09-11 22:22:41 -0400

    added webpack and configs

---
+++ client/webpack.config.ts
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    devtool: 'inline-source-map',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+              test: /\.css$/i,
+              include: path.resolve(__dirname, 'src'),
+              use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
